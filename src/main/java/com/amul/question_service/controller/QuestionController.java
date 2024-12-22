2d1e5b92c03bdf7a83ed7bd3b8675cd21a0c4669
Amul Katta <63243603+iamamul@users.noreply.github.com>
2024-12-22 12:47:42 +0530

    implement environment to check the port

--- src/main/java/com/amul/question_service/controller/QuestionController.java
+++ src/main/java/com/amul/question_service/controller/QuestionController.java
@@ -7,6 +7,7 @@ import com.amul.question_service.model.QuestionWrapper;
 import com.amul.question_service.model.Response;
 import com.amul.question_service.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +19,9 @@ public class QuestionController {
 
     @Autowired
     QuestionService questionService;
+
+    @Autowired
+    Environment environment;
 
     @GetMapping("allQuestions")
     public ResponseEntity<List<Question>> getAllQuestions(){
@@ -41,6 +45,7 @@ public class QuestionController {
 
     @PostMapping("getQuestions")
     public ResponseEntity<List<QuestionWrapper>> getQuestionFromId(@RequestBody List<Integer> questionIds){
+        System.out.println(environment.getProperty("local.server.port"));
         return questionService.getQuestionsFromId(questionIds);
     }
 
